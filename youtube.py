dcc14bb51f18850b5166aebfc1f69ad5c51d0cda
Magnificent-Jay <jeremiah.m1801368@st.futminna.edu.ng>
2024-11-19 03:20:34 +0100

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -1,4 +1,25 @@
 import streamlit as st
+import os
+from pytube import Playlist, Youtube
+
+def display_res(yt):
+    """Display available resolutions for a given YouTube video."""
+    print("\nAvailable resolutions:")
+    streams = yt.streams.filter(progressive=True, file_extension="mp4")
+
+    resolutions = []
+    for i, stream in enumerate(streams):
+        print(f"{i + 1}. {stream.resolution}")
+        resolutions.append(stream)
+
+    # Return list of available streams
+    return resolutions
+
+
+
+
+
+
 
 # Set the title of the web app
 st.title("Youtube video downloader")
