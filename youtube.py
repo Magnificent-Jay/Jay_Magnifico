911ed90a9e26e3b6c80b7345b4a693c899c28285
Magnificent-Jay <jeremiah.m1801368@st.futminna.edu.ng>
2024-11-19 01:05:26 +0100

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -5,7 +5,17 @@ st.title("Youtube video downloader")
 
 # Get text input from the user
 user_input = st.text_input("Enter the url of the youtube video:")
+enter = st.button("Convert")
 
+if enter:
+    if user_input:
+        st.write(f"You entered: {user_input}")
+    else:
+        st.write("No input provided.")
+
+"""
 # Display the input back to the user
 if user_input:
     st.write(f"You entered: {user_input}")
+"""
+
