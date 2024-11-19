35095c0ecc671daed85a3a8c118c6a157894195d
Magnificent-Jay <jeremiah.m1801368@st.futminna.edu.ng>
2024-11-19 01:50:16 +0100

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -3,12 +3,12 @@ import streamlit as st
 # Set the title of the web app
 st.title("Youtube video downloader")
 
-# Set the columns of the widgets
+st.write("Enter the url of the youtube video: ")
+
+# Set the columns of the text widget
 col1, col2 = st.columns([10, 1])
 
-
 # Get text input from the user
-st.write("Enter the url of the youtube video: ")
 user_input = col1.text_input()
 enter = col2.button("Convert")
 
